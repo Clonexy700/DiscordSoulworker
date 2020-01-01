86b884160b8d89028e63afb802d1db32c8e07bf5
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2020-01-01 22:56:06 +0700

    Hello World Discord commands

---
+++ backend/cogs/basic.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+class basic(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'{self.client.user} has connected to Discord! I want to play a game!')
+        await self.client.change_presence(activity=discord.Game(name='$help'))
+
+    @commands.command(name='ping', help=' - shows my ping')
+    async def ping(self, ctx):
+        embedping = discord.Embed(
+            color=discord.Colour.dark_purple()
+        )
+        embedping.add_field(name='My ping', value=f'Pong!{round(self.client.latency * 1000)}ms')
+        await ctx.send(embed=embedping)
+
+
+def setup(client):
+    client.add_cog(basic(client))
--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 import discord
+import os
 from discord.ext import commands
 client = commands.Bot(command_prefix='l!')
+
+for filename in os.listdir('./backend/cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'backend.cogs.{filename[:-3]}')
+
 token = 0
-f=open("token.txt", "r")
+f = open("token.txt", "r")
 if f.mode == 'r':
     token = f.read()
+
 
 client.run(token)
