7af5d3f4f9ffec3eb36bb56a688babad96ff0b5e
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2020-01-09 23:41:44 +0700

    Github add

--- backend/cogs/basic.py
+++ backend/cogs/basic.py
@@ -18,6 +18,14 @@ class basic(commands.Cog):
         embedping.add_field(name='My ping', value=f'Pong!{round(self.client.latency * 1000)}ms')
         await ctx.send(embed=embedping)
 
+    @commands.command(name='github', help=' - sends github Lilly Bloommerchen link')
+    async def github(self, ctx):
+        github = discord.Embed(
+            color=discord.Colour.dark_purple()
+        )
+        github.add_field(name='Github', value='https://github.com/Clonexy700')
+        await ctx.send(embed=github)
+
 
 def setup(client):
     client.add_cog(basic(client))
